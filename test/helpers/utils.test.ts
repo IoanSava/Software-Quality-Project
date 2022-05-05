7dc7a719d90ba7a1a3cb842ffe22024e4319c88a
Diana Crainic <dianacrainic801@yahoo.com>
2022-05-05 19:51:33 +0300

    Add unit tests for multiplication, division, util function

--- test/helpers/utils.test.ts
+++ test/helpers/utils.test.ts
@@ -1,4 +1,4 @@
-import { doesStringContainOnlyDigits, isFalsy } from "../../src/helpers/utils";
+import { doesStringContainOnlyDigits, isFalsy, transformValuesToMap } from "../../src/helpers/utils";
 
 describe("doesStringContainOnlyDigits", () => {
     test("should return true when an empty string is given", () => {
@@ -37,5 +37,66 @@ describe("isFalsy", () => {
 
     test("should return false when a string is given", () => {
         expect(isFalsy("test")).toBe(false);
+    });
+});
+
+describe("transformValuesToMap", () => {
+    test("should return an empty map when undefined is given", () => {
+        let expectedMap: Map<string, string> = new Map();
+        expect(transformValuesToMap(undefined)).toEqual(expectedMap);
+    });
+
+    test("should return an empty map when an empty string is given", () => {
+        let expectedMap: Map<string, string> = new Map();
+        expect(transformValuesToMap("")).toEqual(expectedMap);
+    });
+
+    test("should return an empty map with when a string containig only a variable is given", () => {
+        let values = "a";
+        let expectedMap: Map<string, string> = new Map();
+        expect(transformValuesToMap(values)).toEqual(expectedMap);
+    });
+
+    test("should return an empty map when a string containig multiple variables without values is given", () => {
+        let values = "x,y,z";
+        let expectedMap: Map<string, string> = new Map();
+        expect(transformValuesToMap(values)).toEqual(expectedMap);
+    });
+
+    test("should return a map with a single tuple when a string with multiple commas is given", () => {
+        let values = "test=8365,,,,";
+        let expectedMap: Map<string, string> = new Map();
+        expectedMap.set("test", "8365");
+        expect(transformValuesToMap(values)).toEqual(expectedMap);
+    });
+
+    test("should return a map with a single tuple when a string with commas and equal signs is given", () => {
+        let values = "test=87402,=,,=,";
+        let expectedMap: Map<string, string> = new Map();
+        expectedMap.set("test", "87402");
+        expect(transformValuesToMap(values)).toEqual(expectedMap);
+    });
+
+    test("should return an empty map when a string with only the variable is given", () => {
+        let values = "test=,";
+        let expectedMap: Map<string, string> = new Map();
+        expect(transformValuesToMap(values)).toEqual(expectedMap);
+    });
+
+    test("should return a map with the corresponding variables and values when a string containig variables, values and spaces is given", () => {
+        let values = "  a=123,  b=456,   c=789 ";
+        let expectedMap: Map<string, string> = new Map();
+        expectedMap.set("a", "123");
+        expectedMap.set("b", "456");
+        expectedMap.set("c", "789");
+        expect(transformValuesToMap(values)).toEqual(expectedMap);
+    });
+
+    test("should return a map with the corresponding variables and values when a string containig variables and values is given", () => {
+        let values = "first=630,second=19273";
+        let expectedMap: Map<string, string> = new Map();
+        expectedMap.set("first", "630");
+        expectedMap.set("second", "19273");
+        expect(transformValuesToMap(values)).toEqual(expectedMap);
     });
 });
